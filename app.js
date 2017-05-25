0421248de5304fed74efc4b4fcc17ae0d43315a9
Robert Burton <dalek42@bubble.org>
2017-05-24 23:46:40 -0400

    Started implementing a section to add apps to the addbox

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ console.log("JavaScript File Entered");
 
 var main = function() {
 	console.log("Running Main");
+	test();
 };
 
 var test = function() {
-	
+	chrome.storage.sync.set({
+		"var1" : 5,
+		"var2" : 10
+	}, function() {});
+	chrome.storage.sync.get("var1", function(data) {
+		console.log(data.var1);
+	});
+	chrome.storage.sync.get("var2", function(dataz) {
+		console.log(dataz.var2);
+	});
 };
+
+var addApp = function(nname,nurl,nimage) {
+	var box = document.getElementById("appbox");
+	var app = document.createElement("div");
+	
+}
 
 window.onload = function(){
 	console.log("Window Onload Called");
