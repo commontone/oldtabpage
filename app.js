f8ff6a38c5d7de4cec953f6af12c5510b3a341e3
Robert Burton <dalek42@bubble.org>
2017-05-25 23:35:25 -0400

    Apps can now be created from a basic script

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-console.log("JavaScript File Entered");
+//Old Tab Page Chrome Extension
+//Application Javascript File
+//Author: Robert Burton https://www.robert.red
+//MIT License
 
+console.log("JavaScript File Entered");
+var apps = [
+	{appname : "Google", appurl : "https://www.google.com", appimage : "google.png", lastClicked : new Date(), numClicks : 0}
+];
 var main = function() {
+	
 	console.log("Running Main");
 	test();
 };
+
+var buildApp = function(index) {
+	var built = "";
+	built = built + 
+		"<div class=\"app\"><a href=\""
+		+apps[index].appurl
+		+"\"><img src=\""
+		+apps[index].appimage
+		+"\"></a></div>";
+	return built;
+}
 
 var test = function() {
 	chrome.storage.sync.set({
@@ -16,12 +35,26 @@ var test = function() {
 	chrome.storage.sync.get("var2", function(dataz) {
 		console.log(dataz.var2);
 	});
+	addApp(0,0,0);
+	console.log(apps[0]);
+	console.log(buildApp(0));
+	var box = document.getElementById("appbox");
+	box.innerHTML = box.innerHTML + buildApp(0);
+	
 };
 
 var addApp = function(nname,nurl,nimage) {
 	var box = document.getElementById("appbox");
 	var app = document.createElement("div");
-	
+	app.className = "app";
+	var ref = document.createElement("a");
+	ref.href = "#";
+	var theimage = document.createElement("img");
+	theimage.src = "gradient.png";
+	ref.appendChild(theimage);
+	app.appendChild(ref);
+	box.appendChild(app);
+	console.log("test");
 }
 
 window.onload = function(){
